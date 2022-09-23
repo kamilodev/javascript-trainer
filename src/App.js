659d729d127d08799c61a6ac504d1384ad91a1eb
ProKav <kamilo_vanegas@hotmail.com>
2022-09-23 23:02:15 +0200

    Responsive features included

--- src/App.js
+++ src/App.js
@@ -4,33 +4,33 @@ import './styles/styles.css';
 function App() {
 	return (
 		<>
-			<div class='container'>
-				<div class='header'>
-					<div class='game-title'>Game title</div>
-					<div class='dark-mode'>Dark Mode</div>
-					<div class='language-mode'>Language</div>
+			<div className='container'>
+				<div className='header'>
+					<div className='game-title'>Game title</div>
+					<div className='dark-mode'>Dark Mode</div>
+					<div className='language-mode'>Language</div>
 				</div>
-				<div class='footer'>
-					<div class='made-love'>Made With Love</div>
+				<div className='footer'>
+					<div className='made-love'>Made With Love</div>
 				</div>
-				<div class='game-area'>
-					<div class='player-name'>Player Name</div>
-					<div class='player-points'>Player Points</div>
-					<div class='counter'>Counter</div>
-					<div class='lives'>
-						<div class='heart-1'>Heart 1</div>
-						<div class='heart-2'>Heart 2</div>
-						<div class='heart-3'>Heart 3</div>
+				<div className='game-area'>
+					<div className='player-name'>Player Name</div>
+					<div className='player-points'>Player Points</div>
+					<div className='counter'>Counter</div>
+					<div className='lives'>
+						<div className='heart-1'>Heart 1</div>
+						<div className='heart-2'>Heart 2</div>
+						<div className='heart-3'>Heart 3</div>
 					</div>
-					<div class='card-play'>Card</div>
-					<div class='messages'>Message</div>
-					<div class='buttons'>
-						<div class='option-A'>Option A</div>
-						<div class='option-B'>Option B</div>
-						<div class='option-C'>Option C</div>
+					<div className='card-play'>Card</div>
+					<div className='messages'>Message</div>
+					<div className='buttons'>
+						<div className='option-A'>Option A</div>
+						<div className='option-B'>Option B</div>
+						<div className='option-C'>Option C</div>
 					</div>
-					<div class='start-control'>
-						<div class='button-control'>Button Control</div>
+					<div className='start-control'>
+						<div className='button-control'>Button Control</div>
 					</div>
 				</div>
 			</div>
